8c6ac17ffee968cf6c93ac079f2a4d87a010555f
300915966 <300915966@PR-A315-18>
2017-07-25 15:32:39 -0400

    Created Deck Class

---
+++ Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Comp123_S2017_Lesson11A
+{
+    public class Card
+    {
+        //private instance variables
+        private Face _face;
+        private Suits _suit;
+        //public properties
+        public Face Face
+        {
+            get
+            {
+                return this._face;
+            }
+            set
+            {
+                this._face = value;
+            }
+        }
+            public Suits Suits
+        {
+            get
+            {
+                return this._suit;
+
+            }
+            set
+            {
+                this._suit = value;
+            }
+        }
+
+        }
+    }
---
+++ Deck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Comp123_S2017_Lesson11A
+{
+    public class Deck:List<Card>
+    {
+        //private instance variable
+      
+        //public prop
+
+        //constructors
+
+        //
+    }
+}
