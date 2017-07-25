df52a2250ec14d6484c572d1f12c3325a1e05c26
300915966 <300915966@PR-A315-18>
2017-07-25 15:18:48 -0400

    Enum Class

---
+++ Face.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Kirandeep Kaur
+ * Date: jULY 25 2017
+ * Description:Enumeration for Face
+ * Version:0.1-Created the Face Enum
+ */
+namespace Comp123_S2017_Lesson11A
+{
+    public enum Face
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
--- Suits.cs
+++ Suits.cs
@@ -9,14 +9,13 @@ using System.Threading.Tasks;
  * Description:Enumeration for Suits
  * Version:0.1-Created the Suit Enum
  */
-
 namespace Comp123_S2017_Lesson11A
 {
-    class Program
+   public enum Suits
     {
-        static void Main(string[] args)
-        {
-
-        }
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
     }
 }
